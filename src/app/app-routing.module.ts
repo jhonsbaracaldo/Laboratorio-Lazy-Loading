65d4b04c7d2b533b200da539d16dcbf80c990817
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2024-01-19 17:13:56 -0500

    probando Lazy-loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
-
+ const routes: Routes = [
+  {
+   path: '',
+   component: HomeComponent
+ },
+  { path: 'AboutUs', loadChildren: () => import('./about-us/about-us.module').then(m => m.AboutUsModule) },
+  { path: 'Contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
